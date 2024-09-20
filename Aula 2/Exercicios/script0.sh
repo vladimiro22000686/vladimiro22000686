90f00f559fa937488cdf97b3368cba62bb5cdef0
vladimiro <a22000686@alunos.ulht.pt>
2024-09-20 12:47:43 +0100

    added argument persing

--- Aula 2/Exercicios/script0.sh
+++ Aula 2/Exercicios/script0.sh
@@ -6,3 +6,15 @@ echo "hello world" #hello world!
 
 s="hello world 2!"
 echo $s
+#0.2 2 execução de comandos
+pwd
+
+cmd="ls"
+$cmd
+
+#0.3 Built-in variables
+echo "Number of arguments passed to script: $#"
+echo "All arguments passed to script: $@"
+echo "Script's arguments separated into different variables: $1 $2..."
+echo "Last program's return value: $?"
+echo "Script's PID: $$"
